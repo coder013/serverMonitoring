6640f8bd80b5e41bec82f303ced9f1332585fa09
coder013 <wnssh607@gmail.com>
2021-09-27 01:29:58 +0900

    로그인, 회원가입 유효성 검사 추가

--- console/src/main/java/com/sm/console/dto/UserDto.java
+++ console/src/main/java/com/sm/console/dto/UserDto.java
@@ -6,6 +6,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -13,7 +15,9 @@ import lombok.Setter;
 public class UserDto {
 
     private Integer id;
+    @NotBlank(message = "ID를 입력하십시오.")
     private String loginId;
+    @NotBlank(message = "비밀번호를 입력하십시오.")
     private String password;
 
     public User toEntity() {
